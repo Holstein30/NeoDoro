25f4416a780beb7464e1d1cf8f1999c61eea0c72
Dale Holstein <Holstein30@live.com>
2019-12-04 16:23:50 -0500

    Scripts - countdown on click

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // JS
 
 // Variables
-let countdown;
+let countdown = 25 * 60;
 const displayTimer = document.querySelector('.display__time-left');
+const timerButton = document.querySelector('.timer__button');
 
-displayTimer.textContent = '11';
+let renderTimer = () => {
+    displayTimer.textContent = countdown;
+};
+
+timerButton.addEventListener('click', () => {
+    setInterval(countdown--, 1000);
+    renderTimer();
+});
