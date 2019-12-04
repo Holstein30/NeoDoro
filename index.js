0107daa18722e5f340b85876f22023b121071317
Dale Holstein <Holstein30@live.com>
2019-12-04 16:24:58 -0500

    Scripts - timer counting down after start clicked

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let countdown = 25 * 60;
 const displayTimer = document.querySelector('.display__time-left');
 const timerButton = document.querySelector('.timer__button');
 
-let renderTimer = () => {
+let timer = () => {
+    countdown--;
     displayTimer.textContent = countdown;
 };
 
 timerButton.addEventListener('click', () => {
-    setInterval(countdown--, 1000);
-    renderTimer();
+    setInterval(timer, 1000);
 });
