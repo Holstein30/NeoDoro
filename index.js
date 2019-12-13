bb249108e104396ed87efd89b16de590cdc54245
Dale Holstein <holstein30@live.com>
2019-12-12 19:32:41 -0500

    full refactor

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// JS
-
-// Variables
 let countdown;
-let time = 5;
-const displayTimer = document.querySelector(".display__time-left");
-const timerControls = document.querySelector(".timer__controls");
-const timerButton = document.querySelector(".timer__button");
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
-let timer = seconds => {
+function timer(seconds) {
+  // clear any existing timers
   clearInterval(countdown);
 
   const now = Date.now();
@@ -17,25 +14,35 @@ let timer = seconds => {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-
+    // check if we should stop it!
     if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-
+    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
-};
+}
 
-let displayTimeLeft = secondsLeft => {
-  const minutes = Math.floor(secondsLeft / 60);
-  const remainderSeconds = secondsLeft % 60;
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
   const display = `${minutes}:${
     remainderSeconds < 10 ? "0" : ""
   }${remainderSeconds}`;
   document.title = display;
-  displayTimer.textContent = display;
-};
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+}
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
@@ -43,10 +50,10 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener("click", startTimer));
-
-timerControls.addEventListener("click", e => {
+document.customForm.addEventListener("submit", function(e) {
   e.preventDefault();
-  displayTimer.textContent = "25:00";
-  timerButton.textContent = "RESET";
-  timer();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
 });
